3ebbd9a843ee420646d729d78bfd559637d3be04
Damian Przybyła <przybyladamian.98@gmail.com>
2023-11-15 04:44:12 +0100

    new subpage all_comments with possible to choose a comment account, change field author from a text field to a foreign key from built-in auth, fixed code with author field changes, auto set logged account as author of article

--- Article/models.py
+++ Article/models.py
@@ -1,18 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 
 class Article(models.Model):
     title = models.CharField(max_length=120)
-    author = models.CharField(max_length=50)
+    # author = models.CharField(max_length=50)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField(blank=False, null=False)
     priority = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
 
 
 class Comment(models.Model):
     article = models.ForeignKey(Article, on_delete=models.CASCADE)
-    author = models.CharField(max_length=100)
+    # author = models.CharField(max_length=100)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
 
--- Article/views.py
+++ Article/views.py
@@ -2,12 +2,13 @@ from django.shortcuts import render, get_object_or_404, redirect
 
 from .models import Article, Comment
 from django.views.generic.edit import CreateView
+from django.contrib.auth.models import User
 
-from .forms import ArticleForm
+from .forms import ArticleForm, OptionForComments
 
 
 def article_list_view(request):
-    articles = Article.objects.all().order_by('-priority')
+    articles = Article.objects.all().order_by('-priority', '-created_at')
     comments = Comment.objects.all().order_by('-created_at')
     context = {
         'object_list': articles,
@@ -19,7 +20,7 @@ def article_list_view(request):
 def add_comment(request):
     if request.method == 'POST':
         article_id = request.POST.get('article_id')
-        author = request.POST.get('author')
+        author = request.user
         content = request.POST.get('content')
 
         article = get_object_or_404(Article, pk=article_id)
@@ -36,16 +37,48 @@ def add_comment(request):
 class ArticleCreateView(CreateView):
     model = Article
     template_name = 'articles/create_article.html'
-    fields = ['title', 'author', 'content', 'priority']
+    fields = ['title', 'content', 'priority']
 
 
 def add_article(request):
     if request.method == 'POST':
         form = ArticleForm(request.POST)
         if form.is_valid():
-            form.save()
-            return redirect('article-list')  # Assuming you have a URL named 'article-list'
+            article = form.save(commit=False)
+            article.author = request.user
+            article.save()
+            return redirect('article-list')
     else:
         form = ArticleForm()
 
     return render(request, 'articles/create_article.html', {'form': form})
+
+
+def all_comments(request):
+    # users = User.objects.all()
+
+    if request.method == 'GET':
+        form = OptionForComments(request.GET)
+        # form.files['filter_option'].choices = [(user.id, user.username) for user in users]
+
+        if form.is_valid():
+            selected_user_id = form.cleaned_data['filter_option']
+            if selected_user_id:
+                comments = Comment.objects.filter(author__id=selected_user_id).order_by('-created_at')
+                for a in comments:
+                    print(a)
+            else:
+                comments = Comment.objects.order_by('-created_at')
+
+        else:
+            comments = Comment.objects.order_by('-created_at')
+    else:
+        form = OptionForComments()
+        comments = Comment.objects.order_by('-created_at')
+
+    context = {
+        'comments': comments,
+        'form': form
+    }
+
+    return render(request, 'articles/all_comments.html', context)
