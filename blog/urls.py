d23c3fbdce81061758d89993be72437fdb6166be
Damian Przybyła <przybyladamian.98@gmail.com>
2023-11-16 03:46:28 +0100

    option to delete comment for admin, now only admin can set priority on articles, css

--- blog/urls.py
+++ blog/urls.py
@@ -21,7 +21,14 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.views.generic.base import TemplateView
 
-from Article.views import article_list_view, add_comment, ArticleCreateView, add_article, all_comments, article_view
+from Article.views import (
+    article_list_view,
+    add_comment,
+    ArticleCreateView,
+    add_article, all_comments,
+    article_view,
+    delete_comment
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls, name='admin'),
@@ -32,6 +39,7 @@ urlpatterns = [
     path('create_article/', ArticleCreateView.as_view(), name='create_article'),
     path('add_article/', add_article, name='add_article'),
     path('all_comments/', all_comments, name='all_comments'),
+    path('delete_comment/<int:comment_id>/', delete_comment, name='delete_comment'),
 
 
     path("accounts/", include("accounts.urls")),
